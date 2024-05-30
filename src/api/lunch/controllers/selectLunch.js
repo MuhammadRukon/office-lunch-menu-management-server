208887af30c7d70c4f52606499bdf3dd447f1b7b
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-05-31 05:14:26 +0600

    feat: view choices done

--- src/api/lunch/controllers/selectLunch.js
+++ src/api/lunch/controllers/selectLunch.js
@@ -3,13 +3,14 @@ const { pool } = require("./../../../db/connectDb.js");
 const selectLunch = async (req, res) => {
   const { lunch_id, employee_id } = req.body;
   const values = [lunch_id, employee_id];
-  console.log(lunch_id, employee_id);
-  console.log(values);
   const query = `INSERT INTO employee_choice(lunch_id, employee_id)
     VALUES($1, $2) RETURNING *;`;
+  try {
     const response = await pool.query(query, values);
     res.send(response);
-    console.log(response);
+  } catch (error) {
+    res.send(error);
+  }
 };
 
 module.exports = selectLunch;
--- src/routes/lunch/index.js
+++ src/routes/lunch/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const addMenu = require("../../api/lunch/controllers/addMenu.js");
 const getMenu = require("../../api/lunch/controllers/getMenu.js");
 const selectLunch = require("../../api/lunch/controllers/selectLunch.js");
+const getChoice = require("../../api/lunch/controllers/getChoice.js");
 const router = express.Router();
 
 router.post("/add-menu", addMenu);
 router.post("/menu", getMenu);
 router.post('/select-lunch', selectLunch);
+router.get('/choice', getChoice);
 
 module.exports = router;
